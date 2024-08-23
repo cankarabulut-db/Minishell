f40dcba915bdf8bae900fad46ab6da2cd079f884
Numan Can Karabulut <nkarabul@k1m42s05.42istanbul.com.tr>
2024-08-23 20:10:24 +0300

    güncel

--- main.c
+++ main.c
@@ -6,82 +6,34 @@
 /*   By: nkarabul <nkarabul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:59:11 by nkarabul          #+#    #+#             */
-/*   Updated: 2024/07/24 19:34:44 by nkarabul         ###   ########.fr       */
+/*   Updated: 2024/08/23 18:48:24 by nkarabul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int take_size_env(char **env)
+void start_cmd(char **env)
 {
-	int i;
-
-	i = 0;
-	if(!env)
-		return (i);
-	while(env[i])
-		i++;
-	return (i);
-}
-
-void take_enviro(t_shell *cmds, t_env *env, int st, char **environ)
-{
-	int i;
-
-	i = -1;
-	if(st == 0)
-	{
-		env->env = malloc(sizeof(char *) * take_size_env(environ) + 1);
-		if(!env->env)
-			error_msg("Malloc!\n");
-		while(take_size_env(env->env) > ++i)
-			env->env[i] = ft_strdup(environ[i]);
-		env->env[i] = NULL;
-	}
-	else
-	{
-		cmds->env = malloc(sizeof(char *) * take_size_env(env->env) + 1);
-		if(!cmds->env)
-			error_msg("Malloc!\n");
-		while(take_size_env(env->env) > ++i)
-			cmds->env[i] = ft_strdup(env->env[i]);
-		cmds->env[i] = NULL;
-	}
-}
-void initialize_structs(t_shell *cmd, t_env *env)
-{
-	cmd = malloc(sizeof(t_shell));
-	cmd->main_env = env;
-	take_enviro(cmd,env,1,NULL);
-	cmd->status = 0;
-	cmd->next = NULL;
-}
-
-void start_cmd(char **str)
-{
-	char *rcmd;
 	char *temp;
-	t_shell cmd;
-	t_env *env;
-	
-	take_enviro(NULL, env, 0, str);
+	char *rcmd;
+
+	(void)env;
 	while(1)
 	{
-		initialize_structs(&cmd,env);
-		str = readline("mini##> ");
-		if(!str)
+		rcmd = readline("mini##> ");
+		if(!rcmd)
 		{
-			free(env);
-			printf("minishell: invalid input!\n");
+			printf("exit\n");
 			exit(EXIT_FAILURE);
 		}
-		add_history(str);
+		add_history(rcmd);
 		temp = ft_strtrim(rcmd," \t");
+		start_parse(temp);
 	}
 }
 
-int main(int ac, char *av[]) {
-	extern char **env;
+int main(int ac, char *av[],char **env) {
+	(void)av;
    	if(ac != 1)
 		error_msg("Too much arguments.");
 	start_cmd(env);
--- parsing/ps.c
+++ parsing/ps.c
@@ -3,20 +3,26 @@
 void quote_check(char *str)
 {
 	int i;
+	int q_count;
+	char c;
 
+	q_count = 0;
 	i = 0;
+
+
 	while(str[i])
 	{
-		if(str[i] == '"')
-		{
-			i++;
-			while(str[i] != '"')
-			{
-				if(str[i] == '"')
-					
-			}
+		if ((str[i] == '\"' || str[i] == '\'') && q_count % 2 == 0)
+		{	
+			c = str[i];
+			q_count += 1;
 		}
+		else if (str[i] == c)
+			q_count++;
+		i++;
 	}
+	if (q_count % 2 != 0)
+		printf("Error\n");
 }
 
 void start_parse(char *str)
